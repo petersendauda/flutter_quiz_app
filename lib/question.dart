fbcfedad3a98035e33770fba3a742aea330eb54d
benjithorpe <benthorpe.dev@gmail.com>
2023-01-01 21:06:50 +0000

    Add License

--- lib/question.dart
+++ lib/question.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-  String questionText;
+  final String questionText;
 
   Question(this.questionText);
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      margin: EdgeInsets.all(10),
+      width: double.infinity,
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 25),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
