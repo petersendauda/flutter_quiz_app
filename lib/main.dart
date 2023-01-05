a755cd1c0bd96ae058dd8214b8e823982cf8eeb0
benjithorpe <benthorpe.dev@gmail.com>
2023-01-05 16:30:18 +0000

    move question outside widget and conditional rendering

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import './question.dart';
 import 'answer.dart';
 
@@ -12,6 +13,21 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   var _questionIndex = 0;
 
+  final questions = const [
+    {
+      "question": "What is your favorite color?",
+      "answers": ["Black", "Blue", "Red", "Green"]
+    },
+    {
+      "question": "What is your favorite animal?",
+      "answers": ["Bird", "Cat", "Dog", "Lion"]
+    },
+    {
+      "question": "What is your favorite phone brand?",
+      "answers": ["Tecno", "SamSung", "Item", "IPhone"]
+    }
+  ];
+
   void _answerQuestion() {
     setState(() {
       // if (_questionIndex == 1) {
@@ -19,31 +35,40 @@ class _HomePageState extends State<HomePage> {
       // } else {
       //   _questionIndex = _questionIndex + 1;
       // }
-      _questionIndex == 1 ? _questionIndex = 0 : _questionIndex++;
+      _questionIndex == questions.length - 1
+          ? _questionIndex = 0
+          : _questionIndex++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      "What is your favorite color?",
-      "What is your favorite animal?"
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Quiz App"),
         ),
-        body: Column(
-          children: <Widget>[
-            Question(questions[_questionIndex]),
-            Answer("Answer 1", _answerQuestion),
-            Answer("Answer 2", _answerQuestion),
-            Answer("Answer 3", _answerQuestion),
-            Answer("Answer 4", _answerQuestion),
-          ],
-        ),
+        body: _questionIndex == questions.length - 1
+            ? Column(
+                children: <Widget>[
+                  Question(questions[_questionIndex]["question"] as String),
+                  ...(questions[_questionIndex]["answers"] as List<String>)
+                      .map((answer) {
+                    return Answer(answer, _answerQuestion);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Column(
+                  children: [
+                    Text("No More Questions!!!"),
+                    ElevatedButton(
+                      onPressed: () => _questionIndex = 0,
+                      child: Text("Start Again"),
+                    )
+                  ],
+                ),
+              ),
       ),
     );
   }
