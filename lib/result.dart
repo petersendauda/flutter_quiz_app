1dc7ee7738959939cee68e6c4951ac36d13cc6d1
benjithorpe <benthorpe.dev@gmail.com>
2023-01-06 10:38:21 +0000

    customize result output

--- lib/result.dart
+++ lib/result.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int score;
+  final void Function() restartQuiz;
+
+  Result(this.score, this.restartQuiz);
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         children: [
-          Text("No More Questions!!!"),
-          ElevatedButton(
-            onPressed: () => {},
-            child: Text("Start"),
-          )
+          Text(
+            "Total Score: $score",
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: Colors.black54,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+            onPressed: restartQuiz,
+            child: Text("Restart Questions"),
+          ),
         ],
       ),
     );
